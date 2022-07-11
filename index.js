e65ca5a3505fb07c189e22a8aca5e976d9415ceb
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-11 12:34:13 +0300

    Refactor UI class to a module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Modules
 import Book from './modules/book.js'
 import Store from './modules/store.js'
+import UI from './modules/ui.js'
 
 // Selectors
 const addBtn = document.querySelector('.add-btn');
@@ -13,73 +14,6 @@ const listNavLink = document.querySelector('.list-nav-link');
 const addNavLink = document.querySelector('.add-nav-link');
 const contactNavLink = document.querySelector('.contact-nav-link');
 
-
-// ui operations
-
-class UI {
-  static displayBooks() {
-    const books = Store.getBooks();
-
-    books.forEach((book) => {
-      UI.addBookToList(book);
-    });
-
-    allBooksSection.classList.add('show-section');
-    allBooksSection.classList.remove('hide-section');
-    addBookSection.classList.add('hide-section');
-    contactSection.classList.add('hide-section');
-    listNavLink.children[0].classList.add('active');
-    contactNavLink.children[0].classList.remove('active');
-    addNavLink.children[0].classList.remove('active');
-  }
-
-  static addBookToList(book) {
-    // create book Div
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-
-    // Create title
-    const titleElement = document.createElement('h3');
-    titleElement.innerText = book.title;
-
-    // create Author
-    const authorElement = document.createElement('h3');
-    authorElement.innerText = book.author;
-
-    // Create Remove Btn
-    const removeElement = document.createElement('button');
-    removeElement.classList.add('remove-btn');
-    removeElement.innerText = 'Remove';
-    removeElement.setAttribute('data-id', book.index);
-
-    // Create infoDiv element
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info');
-
-    // create h3
-    const linkElement = document.createElement('h3');
-    linkElement.innerText = 'by';
-
-    infoDiv.appendChild(titleElement);
-    infoDiv.appendChild(linkElement);
-    infoDiv.appendChild(authorElement);
-
-    bookDiv.appendChild(infoDiv);
-    booksList.appendChild(bookDiv);
-    bookDiv.appendChild(removeElement);
-  }
-
-  static clearFields() {
-    document.getElementById('title-input').value = '';
-    document.getElementById('author-input').value = '';
-  }
-
-  static deleteBook(el) {
-    if (el.classList.contains('remove-btn')) {
-      el.parentElement.remove();
-    }
-  }
-}
 
 // Adding Book
 addBtn.addEventListener('click', () => {
