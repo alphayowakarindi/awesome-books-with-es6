23430818321f988d346d71026cd360eac1fb87a0
Alphy Gacheru <alphygacheru@gmail.com>
2022-07-11 13:35:23 +0300

    Import DateTime object from luxon.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import Book from "./modules/book.js";
 import Store from "./modules/store.js";
 import UI from "./modules/ui.js";
+import { DateTime } from "./modules/luxon.js";
+
 
 // Selectors
 const addBtn = document.querySelector(".add-btn");
